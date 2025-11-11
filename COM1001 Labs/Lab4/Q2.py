2f32e8f905626ec58057ee89c2b1457cfd2499b9
Mirza5138 <82450658+Mirza5138@users.noreply.github.com>
2025-11-11 21:58:24 +0300

    Added comments.

--- COM1001 Labs/Lab4/Q2.py
+++ COM1001 Labs/Lab4/Q2.py
@@ -1,14 +1,14 @@
 strg=input("Input: ")
-letterStack=[]
+letterStack=[] #Creates a list to store letters of individual words.
 newString=""
 
 for i in strg:
-    if i==" ":
+    if i==" ": #Adds the letters in the stack to the result string and empties the letter stack if it stumbles upon a space character.
         for j in letterStack:
             newString+=j
         newString+=" "
         letterStack=[]
-    else:
+    else: #Inserts the leter at the start of the stack.
         letterStack.insert(0,i)
 for j in letterStack:
     newString+=j
