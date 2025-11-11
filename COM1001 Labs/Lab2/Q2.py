ab85b7358157e8eb44d6fe0c120f50773d1ef733
Mirza5138 <82450658+Mirza5138@users.noreply.github.com>
2025-11-11 21:51:21 +0300

    Added comments.

--- COM1001 Labs/Lab2/Q2.py
+++ COM1001 Labs/Lab2/Q2.py
@@ -1,7 +1,8 @@
 num=int(input("Enter a number: "))
-cloneNum=num
+cloneNum=num #Makes a duplicate of the input to do the operations on.
 palindrome=0
 
+#Puts the number in reverse using arithmetic.
 while cloneNum>0:
     palindrome=palindrome*10+cloneNum%10
     cloneNum//=10
